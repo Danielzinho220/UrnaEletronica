55c5328e8452e3cf9af8a60f9d08b036aad91614
Danielzinho220 <72422753+Danielzinho220@users.noreply.github.com>
2021-07-26 15:31:46 -0300

    vai

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '10125',
+                nome: 'Pavel silva',
+                partido: 'Avp',
+                fotos:[
+                    {url:'cand4.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '46461',
+                nome: 'joão pottes',
+                partido: 'Cam',
+                fotos:[
+                    {url:'cand3.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '90',
+                nome: 'Everaldo Cruz',
+                partido: 'Dhp',
+                vice: 'José messias',
+                fotos:[
+                    {url:'cand1.jpg', legenda: 'Prefeito'},
+                    {url:'cand2.jpg', legenda: 'Vice-prefeito', small: true}
+                ]
+            },
+            {
+                numero: '12',
+                nome: 'Daniel almeida',
+                partido: 'Pvb',
+                vice: 'messi lovrem',
+                fotos:[
+                    {url:'cand5.jpg', legenda: 'Prefeito'},
+                    {url:'cand6.jpg', legenda: 'Vice-prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
